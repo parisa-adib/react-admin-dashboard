dda37800793ed79098f5125ced56e73a9ada6c85
Parisa Adib <parisaadib78@gmail.com>
2023-07-04 12:55:27 +0330

    dashboard project with react.js

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,11 @@
+import {Box, IconButton, useTheme} from "@mui/material";
+import {useContext} from "react";
+import {ColorModeContext, tokens} from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlineIcon from "@mui/icons-material/LightModeOutlined";
+
+const TopBar = () => {
+	return <div>topbar</div>;
+};
+
+export default TopBar;
