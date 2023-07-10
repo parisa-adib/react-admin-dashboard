cd402b02ca806c271811e4216cbe60f9966a3c52
Parisa Adib <parisaadib78@gmail.com>
2023-07-11 00:15:29 +0330

    change colors

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 import {createContext, useState, useMemo} from "react";
 import {createTheme} from "@mui/material/styles";
 
-// color design tokens
+// color design tokens export
 export const tokens = (mode) => ({
 	...(mode === "dark"
 		? {
@@ -18,13 +18,13 @@ export const tokens = (mode) => ({
 				},
 				primary: {
 					100: "#d0d1d5",
-					200: "#a1a4aa",
-					300: "#727680",
-					400: "#434955",
-					500: "#141b2b",
-					600: "#101622",
-					700: "#0c101a",
-					800: "#080b11",
+					200: "#a1a4ab",
+					300: "#727681",
+					400: "#1F2A40",
+					500: "#141b2d",
+					600: "#101624",
+					700: "#0c101b",
+					800: "#080b12",
 					900: "#040509",
 				},
 				greenAccent: {
@@ -75,13 +75,13 @@ export const tokens = (mode) => ({
 				},
 				primary: {
 					100: "#040509",
-					200: "#080b11",
-					300: "#0c101a",
-					400: "#f2f0f0",
-					500: "#141b2b",
-					600: "#434955",
-					700: "#727680",
-					800: "#a1a4aa",
+					200: "#080b12",
+					300: "#0c101b",
+					400: "#f2f0f0", // manually changed
+					500: "#141b2d",
+					600: "#1F2A40",
+					700: "#727681",
+					800: "#a1a4ab",
 					900: "#d0d1d5",
 				},
 				greenAccent: {
@@ -120,15 +120,15 @@ export const tokens = (mode) => ({
 		  }),
 });
 
-//mui themes settings
+// mui theme settings
 export const themeSettings = (mode) => {
 	const colors = tokens(mode);
-
 	return {
 		palette: {
 			mode: mode,
 			...(mode === "dark"
 				? {
+						// palette values for dark mode
 						primary: {
 							main: colors.primary[500],
 						},
@@ -145,6 +145,7 @@ export const themeSettings = (mode) => {
 						},
 				  }
 				: {
+						// palette values for light mode
 						primary: {
 							main: colors.primary[100],
 						},
@@ -162,39 +163,39 @@ export const themeSettings = (mode) => {
 				  }),
 		},
 		typography: {
-			fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+			fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 			fontSize: 12,
 			h1: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 40,
 			},
 			h2: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 32,
 			},
 			h3: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 24,
 			},
 			h4: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 20,
 			},
 			h5: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 16,
 			},
 			h6: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 14,
 			},
 		},
 	};
 };
 
-//context for color mode
+// context for color mode
 export const ColorModeContext = createContext({
-	toggleColormode: () => {},
+	toggleColorMode: () => {},
 });
 
 export const useMode = () => {
@@ -202,11 +203,12 @@ export const useMode = () => {
 
 	const colorMode = useMemo(
 		() => ({
-			toggleColormode: () =>
+			toggleColorMode: () =>
 				setMode((prev) => (prev === "light" ? "dark" : "light")),
 		}),
 		[]
 	);
+
 	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 	return [theme, colorMode];
 };
