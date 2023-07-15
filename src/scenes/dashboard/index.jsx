0cb60fcc3c565659043caa6b9c8f66d9344844f7
Parisa Adib <parisaadib78@gmail.com>
2023-07-15 12:32:16 +0330

    show title and subTitle

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
+import Header from "../../component/Header";
+import {Box} from "@mui/material";
 
 const Dashboard = () => {
-    return (
-        <div>
-           Dashboard
-         
-        </div>
-    );
+	return (
+		<Box m="20px">
+			<Box display="flex" justifyContent="space-between" alignItems="">
+				<Header title="DASHBOARD" subtitle="welcome to your dashboard" />
+			</Box>
+		</Box>
+	);
 };
 
 export default Dashboard;
