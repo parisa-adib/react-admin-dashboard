8bec48c211b0b9216654483c5bcb140abbbe17bb
Parisa Adib <parisaadib78@gmail.com>
2023-07-16 21:42:57 +0330

    import invoices

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import {Route, Routes} from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import SidebarComponent from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/Team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contact";
+import Team from "./scenes/team/index";
+import Invoices from "./scenes/invoices/index";
+import Contacts from "./scenes/contacts/index";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -28,10 +28,10 @@ function App() {
 						<Topbar />
 						<Routes>
 							<Route path="/" element={<Dashboard />} />
-							{/* <Route path="/team" element={<Team />} />
+							<Route path="/team" element={<Team />} />
 							<Route path="/invoices" element={<Invoices />} />
 							<Route path="/contacts" element={<Contacts />} />
-							<Route path="/bar" element={<Bar />} />
+							{/* <Route path="/bar" element={<Bar />} />
 							<Route path="/form" element={<Form />} />
 							<Route path="/line" element={<Line />} />
 							<Route path="/pie" element={<Pie />} />
