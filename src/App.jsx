a6d63e1c798e2d43733b3d21f28bf500982b724e
Parisa Adib <parisaadib78@gmail.com>
2023-07-24 22:46:37 +0330

    Form & Calendar added

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team/index";
 import Invoices from "./scenes/invoices/index";
 import Contacts from "./scenes/contacts/index";
+import Form from "./scenes/form/index";
+import Calendar from "./scenes/calendar/index";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
 
 function App() {
 	const [theme, colorMode] = useMode();
@@ -31,13 +31,13 @@ function App() {
 							<Route path="/team" element={<Team />} />
 							<Route path="/invoices" element={<Invoices />} />
 							<Route path="/contacts" element={<Contacts />} />
-							{/* <Route path="/bar" element={<Bar />} />
 							<Route path="/form" element={<Form />} />
+							<Route path="/calendar" element={<Calendar />} />
+							{/* <Route path="/bar" element={<Bar />} />
 							<Route path="/line" element={<Line />} />
 							<Route path="/pie" element={<Pie />} />
 							<Route path="/fAQ" element={<FAQ />} />
-							<Route path="/geography" element={<Geography />} />
-							<Route path="/calendar" element={<Calendar />} /> */}
+							<Route path="/geography" element={<Geography />} /> */}
 						</Routes>
 					</main>
 				</div>
